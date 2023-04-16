4fa4d2b03f2e5526905b2d736f5016b8778a1ac3
Silvanus Matiku <matiku.silvanus@abc.net.au>
2023-04-16 14:41:49 +1000

    update transcoderMessageModel

--- transcodermessagemodel.go
+++ transcodermessagemodel.go
@@ -3,11 +3,13 @@ package messagemodels
 import "encoding/json"
 
 type TranscoderMessageModel struct {
-	JobID               string
-	RenditionID         string
-	InputMediaFileName  string
-	OutputMediaFileName string
-	TranscodeOption     string
+	JobID           string
+	RenditionID     string
+	InputBucket     string
+	InputKey        string
+	OutputBucket    string
+	OutputKey       string
+	TranscodeOption string
 }
 
 // Unmarshal will attempt to load a json.RawMessage into this object.
